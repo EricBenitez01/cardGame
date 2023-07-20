885d52ba3b0fef044e70d73282250e9c0994ade4
EricBenitez01 <leonetkd@gmail.com>
2023-07-20 14:56:38 -0300

    Arquitectura

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RateComponent } from './components/rate/rate.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardsListComponent,
+    NavComponent,
+    RateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
